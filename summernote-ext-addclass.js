e8b8c139ca106cfaad23604ffd41c149c1e7be5f
Stefano Gargiulo <info@creativeprogramming.it>
2016-02-08 05:16:08 +0100

    Update summernote-ext-addclass.js

--- summernote-ext-addclass.js
+++ summernote-ext-addclass.js
@@ -28,20 +28,23 @@
          */
         'addclass': function (context) {
             var self = this;
-            if (typeof context.options.classTags === 'undefined') {
-                context.options.classTags = [
+            if (typeof context.options.addclass === 'undefined'){
+                   context.options.addclass={};
+            }
+            if (typeof context.options.addclass.classTags === 'undefined') {
+                context.options.addclass.classTags = [
                     //bootstrap3
                     'label', 'img-responsive', 'img-rounded', 'img-thumbnail', 'img-circle', 'sr-only', 'lead', 'text-muted', 'text-primary', 'text-warning', 'text-danger', 'text-success', 'text-info', 'text-left', 'text-right', 'text-center', 'page-header', 'list-unstyled', 'list-inline', 'initialism', 'pull-right', 'prettyprint', 'pre-scrollable', 'container', 'table-bordered', 'table-responsive', 'form-control', 'form-group', 'input-sm', 'input-lg', 'control-label', 'form-control-static', 'help-block', 'btn', 'active', 'btn-default', 'btn-primary', 'btn-warning', 'btn-danger', 'btn-success', 'btn-info', 'btn-link', 'btn-lg', 'btn-xs', 'btn-block', 'fade', 'in', 'collapse', 'collapsing', 'breadcrumb', 'label-default', 'label-primary', 'label-success', 'label-info', 'label-warning', 'label-danger', 'badge', 'jumbotron', 'thumbnail', 'caption', 'alert', 'alert-link', 'alert-dismissable', 'close', 'alert-success', 'alert-info', 'alert-warning', 'alert-danger', 'panel', 'arrow', 'carousel', 'carousel-inner', 'visible-xs', 'visible-sm', 'visible-md', 'visible-lg', 'hidden-xs', 'hidden-sm', 'hidden-md', 'hidden-lg', 'visible-print', '.hidden-print'
                     ,
                     //uikit
                     'uk-button', 'uk-button uk-button-large', 'uk-button uk-button-success', 'uk-button uk-button-primary', 'uk-button uk-button-warning', 'uk-button uk-button-danger'];
-                //  console.log("Please define your summernote.options.classTags array");
+                //  console.log("Please define your summernote.options.addclass.classTags array");
             }
             // ui has renders to build ui elements.
             //  - you can create a button with `ui.button`
             var ui = $.summernote.ui;
             
-            addStyleString(".scrollable-menu {height: auto; max-height: 200px;overflow-x: hidden;}");
+            addStyleString(".scrollable-menu {height: auto; max-height: 200px; max-width:300px; overflow-x: hidden;}");
 
             context.memo('button.addclass', function () {
                 return ui.buttonGroup([
@@ -74,6 +77,9 @@
 
                             event.preventDefault();
                             value = value || $(event.target).closest('[data-value]').data('value');
+                            if (typeof context.options.addclass.debug !== 'undefined' && context.options.addclass.debug){
+                                console.debug($(window.getSelection().focusNode.parentElement),"toggling class: "+value);
+                            }
                             $(window.getSelection().focusNode.parentElement).toggleClass(value)
 
 
